9cffe5d5d483b7ac266d8aa29864af24197772b2
Bartoche <45061154+Bartoche@users.noreply.github.com>
2019-01-31 16:28:45 +0100

    Merge with viven_v2

---
+++ TeamJambeDeBois/Assets/Scripts/TurretContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretContainer : MonoBehaviour {
+
+    public GameObject turrets;
+    private Transform player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        int nbTurret = Random.Range(1, 5);
+        for (int i = 0; i < nbTurret; i++)
+        {
+            GameObject turret = Instantiate(turrets, transform);
+            turret.transform.position = new Vector3(Random.Range(20, 40), 1, Random.Range(-40, 40));
+            turret.transform.rotation = Quaternion.LookRotation(player.position, new Vector3(0, 1, 0));
+        }
+	}
+	
+}
