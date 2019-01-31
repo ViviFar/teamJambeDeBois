790dcfd7f533e68916d21a6de8ec1e6938a7cd6b
Boopyboop <manon.vlieghe@gmail.com>
2019-01-31 14:55:47 +0100

    turret are tracking, projectile script created

---
+++ TeamJambeDeBois/Assets/projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+
+    public float speed = 3.0f;
+    Vector3 m_direction;
+    bool m_fired;
+
+       
+	// Update is called once per frame
+	void Update () {
+		if (m_fired)
+        {
+            transform.position += m_direction * (speed*Time.deltaTime) ;
+        }
+        
+	}
+
+    //lancement d'un projectile
+    void FireProjectile(GameObject launcher, GameObject target, int damage)
+    {
+        
+        if (launcher && target)
+        {
+            m_direction = (target.transform.position - launcher.transform.position).normalized;
+            m_fired = true;
+
+        }
+    }
+}
---
+++ TeamJambeDeBois/Assets/testTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testTarget : MonoBehaviour {
+    /*
+    public Transform pointA;
+    public Transform pointB;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA.position, pointB.position, Mathf.Sin(Time.time * speed));
+    }*/
+}
+
