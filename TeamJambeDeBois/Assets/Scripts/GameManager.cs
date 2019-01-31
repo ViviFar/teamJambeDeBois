4ed7be2504937b94654ada3e89e6bfc75f91d973
ViviFar <vivien.f@live.fr>
2019-01-31 15:58:44 +0100

    merge d'une des versions de Bartoche (dernier commit avant le 31/01/2019 16h) et de vivien_branch

---
+++ TeamJambeDeBois/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<Country> listCountry;
+    int currentCountry = 0;
+
+
+    private void Start()
+    {
+        for(int i=1; i<listCountry.Count; i++)
+        {
+            listCountry[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //To modify to use Oculus input instead of mouse left click on
+        if (Input.GetMouseButtonDown(0))
+        {
+            listCountry[currentCountry].gameObject.SetActive(false);
+            currentCountry++;
+            if (currentCountry >= listCountry.Count)
+            {
+                currentCountry = 0;
+            }
+            listCountry[currentCountry].gameObject.SetActive(true);
+        }
+	}
+}
---
+++ TeamJambeDeBois/Assets/Scripts/cursorBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorBehavior : MonoBehaviour
+{ 
+    private float temperature;
+    private float dryness;
+    private float pollution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y, 0);
+        transform.localPosition = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(temperature>20)
+        {
+            float speedCursor = 0.00625f * temperature - 0.125f;    //
+
+        }
+    }
+}
